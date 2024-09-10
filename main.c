f782fa9f449a10c057eb0119803a0e9853d8ef62
LudoProvost <ludovic.provost8@gmail.com>
2024-09-10 00:47:28 -0400

    started pheader parsing

--- main.c
+++ main.c
@@ -2,29 +2,59 @@
 #include <efilib.h>
 #include <elf.h>
 
-// used to debug the 0x800000000000000E error code received from EFI_FILE_PROTOCOL->Open
+
 const CHAR16* EfiStatusToStr(EFI_STATUS s) {
     switch (s) {
         case EFI_SUCCESS:
             return L"EFI_SUCCESS";
-        case EFI_NOT_FOUND:
-            return L"EFI_NOT_FOUND";
+        case EFI_LOAD_ERROR:
+            return L"EFI_LOAD_ERROR";
+        case EFI_INVALID_PARAMETER:
+            return L"EFI_INVALID_PARAMETER";
+        case EFI_UNSUPPORTED:
+            return L"EFI_UNSUPPORTED";
+        case EFI_BAD_BUFFER_SIZE:
+            return L"EFI_BAD_BUFFER_SIZE";
+        case EFI_BUFFER_TOO_SMALL:
+            return L"EFI_BUFFER_TOO_SMALL";
+        case EFI_NOT_READY:
+            return L"EFI_NOT_READY";
+        case EFI_DEVICE_ERROR:
+            return L"EFI_DEVICE_ERROR";
+        case EFI_WRITE_PROTECTED:
+            return L"EFI_WRITE_PROTECTED";
+        case EFI_OUT_OF_RESOURCES:
+            return L"EFI_OUT_OF_RESOURCES";
+        case EFI_VOLUME_CORRUPTED:
+            return L"EFI_VOLUME_CORRUPTED";
+        case EFI_VOLUME_FULL:
+            return L"EFI_VOLUME_FULL";
         case EFI_NO_MEDIA:
             return L"EFI_NO_MEDIA";
         case EFI_MEDIA_CHANGED:
             return L"EFI_MEDIA_CHANGED";
-        case EFI_DEVICE_ERROR:
-            return L"EFI_DEVICE_ERROR";
-        case EFI_VOLUME_CORRUPTED:
-            return L"EFI_VOLUME_CORRUPTED";
-        case EFI_WRITE_PROTECTED:
-            return L"EFI_WRITE_PROTECTED";
+        case EFI_NOT_FOUND:
+            return L"EFI_NOT_FOUND";
         case EFI_ACCESS_DENIED:
             return L"EFI_ACCESS_DENIED";
-        case EFI_OUT_OF_RESOURCES:
-            return L"EFI_OUT_OF_RESOURCES";
-        case EFI_VOLUME_FULL:
-            return L"EFI_VOLUME_FULL";
+        case EFI_NO_RESPONSE:
+            return L"EFI_NO_RESPONSE";
+        case EFI_NO_MAPPING:
+            return L"EFI_NO_MAPPING";
+        case EFI_TIMEOUT:
+            return L"EFI_TIMEOUT";
+        case EFI_NOT_STARTED:
+            return L"EFI_NOT_STARTED";
+        case EFI_ALREADY_STARTED:
+            return L"EFI_ALREADY_STARTED";
+        case EFI_ABORTED:
+            return L"EFI_ABORTED";
+        case EFI_ICMP_ERROR:
+            return L"EFI_ICMP_ERROR";
+        case EFI_TFTP_ERROR:
+            return L"EFI_TFTP_ERROR";
+        case EFI_PROTOCOL_ERROR:
+            return L"EFI_PROTOCOL_ERROR";
         default:
             return L"Unknown EFI_STATUS";
     }
@@ -65,6 +95,12 @@ EFI_FILE_PROTOCOL* LoadFile(CHAR16* path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TAB
     return loadedFile;
 }
 
+int eHeaderFormatCheck(Elf64_Ehdr* ehdr) {
+    int statusCode = 0;
+
+    //TODO add format check
+}
+
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
 	EFI_STATUS Status;
 	EFI_INPUT_KEY Key;
@@ -75,40 +111,66 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
 
     EFI_FILE_PROTOCOL* kernel = LoadFile(L"\\efi\\boot\\kernel.elf", ImageHandle, SystemTable); 
     if (kernel == NULL) {
-        Print(L"kernel did not load");
+        Print(L"kernel did not load\n");
     } 
     else {
-        Print(L"kernel loaded successfully");
+        Print(L"kernel loaded successfully\n");
     }
 
+    // get kernel.elf header
     Elf64_Ehdr header;
     {
         UINTN kernelInfoSize;
         EFI_FILE_INFO* kernelInfo;
+        EFI_STATUS s;
 
         uefi_call_wrapper(kernel->GetInfo, 4, kernel, &gEfiFileInfoGuid, &kernelInfoSize, NULL);     // get size of kernel info
-        uefi_call_wrapper(SystemTable->BootServices->AllocatePool, 3, EfiLoaderData, kernelInfoSize, (void**) &kernelInfo);    // alloc size for kernel info
-        uefi_call_wrapper(kernel->GetInfo, 4, kernel, &gEfiFileInfoGuid, &kernelInfoSize, (void**) &kernelInfo);    // store kernel info in allocated memory
         
+        uefi_call_wrapper(SystemTable->BootServices->AllocatePool, 3, EfiLoaderData, kernelInfoSize, (void**) &kernelInfo);    // alloc size for kernel GetInfo
+        
+        uefi_call_wrapper(kernel->GetInfo, 4, kernel, &gEfiFileInfoGuid, &kernelInfoSize, (void*) kernelInfo);    // store kernel info in allocated memory
 
-        uefi_call_wrapper(kernel->Read, 3, kernel, (UINTN*) sizeof(header), &header);     // store kernel.elf eheader inside the header var
+        UINTN bufferSize = sizeof(header);
+        s = uefi_call_wrapper(kernel->Read, 3, kernel, &bufferSize, (void*) &header);     // store pointer to kernel.elf eheader inside header var
+        Print(L"%s\n", EfiStatusToStr(s));
         //TODO add EFI_BUFFER_TOO_SMALL check
         
         uefi_call_wrapper(SystemTable->BootServices->FreePool, 1, kernelInfo);    // return pool mem to system (necessary?)
     }
 
-    //TODO add check for e_machine?
+    //TODO add elf format check
     if (header.e_machine != EM_X86_64) {
-        Print(L"kernel format incorrect");
+        Print(L"kernel format incorrect\n");
     } 
     else {
-        Print(L"kernel format correct");
+        Print(L"kernel format correct\n");
+    }
+    
+    // get kernel.elf program header
+    Elf64_Phdr* pheaders;
+    {
+        EFI_STATUS s;
+        UINT64 bufferSize = header.e_phentsize * header.e_phnum;
+
+        s = uefi_call_wrapper(kernel->SetPosition, 2, kernel, header.e_phoff);
+        Print(L"setpos: %s\n", EfiStatusToStr(s));
+
+        s = uefi_call_wrapper(kernel->Read, 3, kernel, &bufferSize, (void*) pheaders);
+        Print(L"readpheader: %s\n", EfiStatusToStr(s));
+    }
+
+    for (UINT64 n = 0; n < header.e_phnum; n++) {
+        Elf64_Phdr* pheader = (Elf64_Phdr*)((char*)pheaders + (n*header.e_phentsize));
+        
+        // debugging
+        Print(L"pheader: %u\n", pheader);
+        Print(L"p_type: %u\n", pheader->p_type);
     }
 
     //TODO allocate memory for kernel
     //TODO load ELF segments
     //TODO handle segment attributes
-    //TODO set up paging ?
+    //TODO set up paging ?!
     //TODO set up CPU state
     //TODO transfer control to kernel
 
